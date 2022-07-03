1eccbf9ba88215e1c592aab46e8a18d85a6b86bf
Richard <m.samadi054321@gmail.com>
2022-07-03 13:42:24 +0430

    init

---
+++ test.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+
+    sf::Texture texture;
+    if (!texture.loadFromFile("pic.png"))
+    {
+        cout << "err loading file" << endl;
+    }
+
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(sprite);
+        window.display();
+    }
+
+    return 0;
+}
